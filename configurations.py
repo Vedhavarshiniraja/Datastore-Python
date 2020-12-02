43bfa81909c1153a0ebc43723ef95ac801b9a96f
Vedhavarshini <47719678+Vedhavarshiniraja@users.noreply.github.com>
2020-12-02 11:34:47 +0530

    Add files via upload

---
+++ configurations.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 10:57:51 2020
+
+@author: lenovo
+"""
+
+
+# Default datasource directory name.
+DEFAULT_DB_PATH = 'db'
+
+# Datasource file name.
+DEFAULT_DB_NAME = 'db.json'
---
+++ settings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 10:58:41 2020
+
+@author: lenovo
+"""
+
+
+# set DEBUG = False when running on production server.
+DEBUG = False
+
+# Sets the host for the flask server.
+HOST = 'localhost'
+
+# Sets the port for the flask server.
+PORT = 5000
+
+# Secret Key for data-encryption/user-authentication.
+SECRET_KEY = 'e8c241g6ea6d4af4b29375bdc5xn27fa'
